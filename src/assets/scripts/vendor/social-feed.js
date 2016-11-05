7b3671ae9f05e658a295e5cc44b9d49459b57373
Mitchell Warren <hello@liquidvisual.com>
2016-11-06 10:15:50 +1100

    Update

---
+++ src/assets/scripts/vendor/social-feed.js
@@ -0,0 +1,51 @@
+/*
+    SOCIAL-FEED.JS - Last updated: 02.11.16
+*/
+//-----------------------------------------------------------------
+// SOCIAL FEED
+// https://github.com/pavelk2/social-feed
+//-----------------------------------------------------------------
+/*
+$(window).on('load', function (){
+
+    $('.social-feed-container').socialfeed({
+        // FACEBOOK
+        facebook: {
+            accounts: ['@elouerasurf'],
+            limit: 4,
+            access_token: '1142564202455221|3a2db052ca1e71a576329b99b7d55a5b'
+        },
+        // INSTAGRAM
+        instagram: {
+            accounts: ['@eloueraslsc'],
+            limit: 4,
+            //client_id: '88b4730e0e2c4b2f8a09a6184af2e218',
+            //access_token: ''
+            client_id: 'aa2ef3e8118541c0b7c372cfd81e752b',
+            access_token: '3178942332.e029fea.ab1887d8d8024dd69d8b1dafbbfa773b'
+        },
+
+        // GENERAL SETTINGS
+        length: 200,
+        show_media: true,
+        template: "/social-feed/",
+
+        // When all the posts are collected and displayed - this function is evoked
+        callback: function () {
+            console.log('all posts are collected');
+            setTimeout(function(){
+              $('.social-feed-container').masonry();
+
+                setTimeout(function(){
+              $('.social-feed-container').masonry();
+            }, 800);
+
+            }, 800);
+        }
+
+    });
+});
+*/
+//==================================================
+//
+//==================================================
